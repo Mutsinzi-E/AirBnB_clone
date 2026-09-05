2b15fed3a2c3ed58c4273649edf3a4b0e2630494
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-09-05 14:11:48 +0000

    Implement models and FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -4,15 +4,28 @@
 import uuid
 from datetime import datetime
 
+from models import storage
+
 
 class BaseModel:
     """Base class for all AirBnB models."""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return the string representation of the instance."""
@@ -21,8 +34,9 @@ class BaseModel:
         )
 
     def save(self):
-        """Update the updated_at attribute."""
+        """Update updated_at and save the object."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance."""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Contains the FileStorage class."""
+
+import json
+
+
+class FileStorage:
+    """Serialize instances to a JSON file and deserialize JSON file."""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary of objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Add an object to the storage dictionary."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serialize objects to the JSON file."""
+        objects_dict = {}
+
+        for key, obj in self.__objects.items():
+            objects_dict[key] = obj.to_dict()
+
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            json.dump(objects_dict, file)
+
+    def reload(self):
+        """Deserialize the JSON file to objects."""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                objects_dict = json.load(file)
+        except FileNotFoundError:
+            return
+
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
+
+        for key, value in objects_dict.items():
+            class_name = value["__class__"]
+            self.__objects[key] = classes[class_name](**value)
